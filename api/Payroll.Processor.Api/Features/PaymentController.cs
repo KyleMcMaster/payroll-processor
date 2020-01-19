264422c891ce83e21c8c0a9e02789da51c215a75
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-01-19 09:55:58 -0500

    feat(payroll): add angular client and .net core api scaffolding

---
+++ api/Payroll.Processor.Api/Features/PaymentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Payroll.Processor.Data.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Payroll.Processor.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Payment> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Payment
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ api/Payroll.Processor.Data.Persistence/Features/Payment.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Payroll.Processor.Data.Persistence
+{
+    public class Payment
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
