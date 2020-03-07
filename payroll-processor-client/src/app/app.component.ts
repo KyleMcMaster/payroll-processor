6d966ae20e57e308a95502aaa1e0a07f4569f592
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-03-06 21:51:45 -0500

    feat(theme): initial dark theme implementation based on bootwatch darkly

--- payroll-processor-client/src/app/app.component.ts
+++ payroll-processor-client/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'payroll-processor-client';
+  title = 'payroll-processor';
   active = 'employees';
 }
