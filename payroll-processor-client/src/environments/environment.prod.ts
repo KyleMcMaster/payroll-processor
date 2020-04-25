76bac43ca131dffa7437a444de2c324f2ec63d44
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-04-25 07:39:18 -0400

    feat(env): remove azure domain, format code

--- payroll-processor-client/src/environments/environment.prod.ts
+++ payroll-processor-client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment-types';
 
 export const environment: Environment = {
   production: true,
-  domain: 'https://nitro-km-payroll-processor.azurewebsites.net'
+  domain: '<your hosting url>',
 };
