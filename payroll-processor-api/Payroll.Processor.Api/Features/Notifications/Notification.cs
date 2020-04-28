27c003377f65f95352944e4a47018d5a7d926a43
Sean Wright <sean@seangwright.me>
2020-04-28 00:52:19 -0400

    feat(api): add signalr and notifications endpoint / hub

---
+++ payroll-processor-api/Payroll.Processor.Api/Features/Notifications/Notification.cs
@@ -0,0 +1,8 @@
+namespace Payroll.Processor.Api.Features.Notifications
+{
+    public class Notification
+    {
+        public string Source { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ payroll-processor-api/Payroll.Processor.Api/Features/Notifications/NotificationHub.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Payroll.Processor.Api.Features.Notifications
+{
+    public class NotificationHub : Hub
+    {
+
+    }
+}
