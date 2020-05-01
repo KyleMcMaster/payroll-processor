ffdb581a395362ea2ebf7698df3f203231244ab0
Sean Wright <sean@seangwright.me>
2020-05-01 02:30:07 -0400

    refactor(app): rename Payroll.Processor to PayrollProcessor to fix namespace/type errors

---
+++ functions/PayrollProcessor.Functions/Features/Resources/ResourceManager.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using PayrollProcessor.Functions.Infrastructure;
+
+namespace PayrollProcessor.Functions.Features.Resources
+{
+    public class ResourceManager
+    {
+        private readonly CloudTableClient tableClient;
+        private readonly CloudQueueClient queueClient;
+
+        public ResourceManager()
+        {
+            string connectionString = EnvironmentSettings
+                .Get("AzureWebJobsAzureTableStorage")
+                .IfNone("UseDevelopmentStorage=true");
+
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            tableClient = storageAccount.CreateCloudTableClient();
+            queueClient = storageAccount.CreateCloudQueueClient();
+        }
+
+        public async Task CreateTable(string tableName)
+        {
+            await tableClient.GetTableReference(tableName).CreateIfNotExistsAsync();
+        }
+
+        public async Task CreateQueue(string queueName)
+        {
+            await queueClient.GetQueueReference(queueName).CreateIfNotExistsAsync();
+        }
+    }
+}
