3bc68ab050e95c85dee439446e349cd04ebcd966
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-06-23 17:35:09 -0400

    feat(employees-refactor): refactor employee-list component using a sfc

--- client/src/app/employee/employee-list/employee-list.component.ts
+++ client/src/app/employee/employee-list/employee-list.component.ts
@@ -4,8 +4,26 @@ import { EmployeeListItem } from './state/employee-list.model';
 
 @Component({
   selector: 'app-employee-list',
-  templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.scss'],
+  template: `
+    <div class="row" *ngFor="let employee of employees">
+      <div
+        class="list-group-item list-group-item-action"
+        (click)="selected.emit(employee)"
+      >
+        <div class="row">
+          <div class="col-10">
+            <span> {{ employee.firstName }} {{ employee.lastName }} </span>
+          </div>
+          <div class="col-2">
+            <fa-icon *ngIf="employee.status === 'Enabled'" [icon]="faSmileBeam">
+            </fa-icon>
+            <fa-icon *ngIf="employee.status === 'Disabled'" [icon]="faSkull">
+            </fa-icon>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
 })
 export class EmployeeListComponent {
   readonly faSkull = faSkull;
@@ -18,8 +36,4 @@ export class EmployeeListComponent {
   selected = new EventEmitter<EmployeeListItem>();
 
   constructor() {}
-
-  setSelected(employee: EmployeeListItem) {
-    this.selected.emit(employee);
-  }
 }
