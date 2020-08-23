d43cd6908ef725c71804a0aefde1b898baf77201
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-08-23 08:22:56 -0700

    feat(employee): add inline template and style

--- client/src/app/employee/employee-payroll-list/employee-payroll-list.component.ts
+++ client/src/app/employee/employee-payroll-list/employee-payroll-list.component.ts
@@ -1,17 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { EmployeePayroll } from '@employee/employee-detail/state/employee.model';
 
 @Component({
   selector: 'app-employee-payroll-list',
-  templateUrl: './employee-payroll-list.component.html',
-  styleUrls: ['./employee-payroll-list.component.scss'],
+  template: ` <h5>Recent Payrolls</h5>
+    <div class="row">
+      <div class="col-6">
+        <ul class="list-group">
+          <li
+            *ngFor="let payroll of payrolls | slice: 0:9"
+            class="list-group-item d-flex justify-content-between payroll-list-item"
+          >
+            <span> {{ payroll.payrollPeriod }} </span>
+            <span> {{ payroll.checkDate | date }} </span>
+            <span class="badge-pill badge-success">
+              {{ payroll.grossPayroll | currency }}
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>`,
+  styles: [
+    `
+      .payroll-list-item {
+        background-color: #000;
+      }
+    `,
+  ],
 })
-export class EmployeePayrollListComponent implements OnInit {
+export class EmployeePayrollListComponent {
   @Input()
-  payrolls: Array<EmployeePayroll>;
+  payrolls: EmployeePayroll[];
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
