eb43db47b8d6bb3e07838ef0514c7ce6a229103d
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-09-07 16:18:40 -0700

    feat(app): alphabetize declarations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,20 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrModule } from 'ngx-toastr';
-
 import { EmployeeCreateComponent } from '@employee/employee-create/employee-create.component';
 import { EmployeeDetailComponent } from '@employee/employee-detail/employee-detail.component';
 import { EmployeeListComponent } from '@employee/employee-list/employee-list.component';
 import { EmployeePayrollListComponent } from '@employee/employee-payroll-list/employee-payroll-list.component';
 import { EmployeeComponent } from '@employee/employee.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PayrollListComponent } from '@payroll/payroll-list/payroll-list.component';
 import { PayrollComponent } from '@payroll/payroll.component';
 import { SharedModule } from '@shared/shared.module';
-
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -25,11 +22,11 @@ import { AppComponent } from './app.component';
     AppComponent,
     EmployeeComponent,
     EmployeeCreateComponent,
+    EmployeeDetailComponent,
     EmployeeListComponent,
+    EmployeePayrollListComponent,
     PayrollComponent,
     PayrollListComponent,
-    EmployeeDetailComponent,
-    EmployeePayrollListComponent,
   ],
   imports: [
     AppRoutingModule,
