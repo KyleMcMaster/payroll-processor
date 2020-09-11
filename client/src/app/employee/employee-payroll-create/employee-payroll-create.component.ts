102ee08b7f9adba0d4164f3d804aea922559042d
Sean Wright <sean@seangwright.me>
2020-09-10 21:43:23 -0400

    fix(client): remove OnChanges in employee-payroll-create, set defaults

--- client/src/app/employee/employee-payroll-create/employee-payroll-create.component.ts
+++ client/src/app/employee/employee-payroll-create/employee-payroll-create.component.ts
@@ -1,33 +1,35 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 import { EmployeeDetail, EmployeePayrollCreate } from '@employee/employee-detail/state/employee-detail.model';
 import { EmployeeDetailService } from '@employee/employee-detail/state/employee-detail.service';
+import { ClockService } from '@shared/clock.service';
 
 @Component({
   selector: 'app-employee-payroll-create',
   templateUrl: './employee-payroll-create.component.html',
   styleUrls: ['./employee-payroll-create.component.scss'],
 })
-export class EmployeePayrollCreateComponent implements OnChanges {
+export class EmployeePayrollCreateComponent {
   filterForm = new FormGroup({
-    checkDate: new FormControl(''),
+    checkDate: new FormControl(this.clock.now().getDate()),
     employeeId: new FormControl(''),
-    grossPayroll: new FormControl(''),
+    grossPayroll: new FormControl(0),
   });
 
   @Input()
   set employee(employee: EmployeeDetail) {
     this.filterForm.patchValue({
       employeeId: employee.id,
+      checkDate: this.clock.now(),
+      grossPayroll: 0,
     });
   }
 
-  constructor(private detailService: EmployeeDetailService) {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.filterForm.reset();
-  }
+  constructor(
+    private detailService: EmployeeDetailService,
+    private clock: ClockService,
+  ) {}
 
   create(): void {
     const payroll: EmployeePayrollCreate = {
