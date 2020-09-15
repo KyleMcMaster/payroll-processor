ce864f71ecd7f4a03ac94c160011831d7bb7e449
Sean Wright <sean@seangwright.me>
2020-09-15 02:14:21 -0400

    feat(client): add admin stats/data generation features

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,7 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+
+import { ResourcesQuery } from './state/resources.query';
+import { ResourcesService } from './state/resources.service';
 
 @Component({
   selector: 'app-admin',
-  template: ` <h1>Admin Component</h1> `,
+  templateUrl: './admin.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminComponent {}
+export class AdminComponent {
+  readonly faSyncAlt = faSyncAlt;
+  readonly isLoading = this.query.selectLoading();
+  readonly stats = this.query.stats;
+
+  readonly form = new FormGroup({
+    totalEmployees: new FormControl(1, { validators: [Validators.min(1)] }),
+    maxPayrolls: new FormControl(1, { validators: [Validators.min(1)] }),
+  });
+
+  constructor(
+    private readonly query: ResourcesQuery,
+    private readonly service: ResourcesService,
+  ) {
+    this.service.getStats();
+  }
+
+  onRefreshStats() {
+    this.service.getStats();
+  }
+
+  onCreate() {
+    this.service.create(
+      this.form.value.totalEmployees,
+      this.form.value.maxPayrolls,
+    );
+  }
+
+  onReset() {
+    this.service.reset();
+  }
+}
