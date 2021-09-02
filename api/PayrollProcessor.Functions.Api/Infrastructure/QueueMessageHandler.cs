25a83e2dca05e49320235a37a6fa1ece6cd4e2ee
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2021-09-01 18:49:26 -0700

    chore(framework): upgrade to .net 6, azure functions v4

--- api/PayrollProcessor.Functions.Api/Infrastructure/QueueMessageHandler.cs
+++ api/PayrollProcessor.Functions.Api/Infrastructure/QueueMessageHandler.cs
@@ -7,7 +7,9 @@ namespace PayrollProcessor.Functions.Api.Infrastructure
     public class QueueMessageHandler
     {
         public static TMessage FromQueueMessage<TMessage>(CloudQueueMessage queueMessage) where TMessage : IMessage =>
+#pragma warning disable CS8603 // Possible null reference return.
             JsonConvert.DeserializeObject<TMessage>(queueMessage.AsString);
+#pragma warning restore CS8603 // Possible null reference return.
 
         public static string GetEventName(CloudQueueMessage queueMessage) =>
             JsonConvert.DeserializeObject<DefaultMessage>(queueMessage.AsString)?.EventName ?? "";
